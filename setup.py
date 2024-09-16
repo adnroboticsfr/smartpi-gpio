04fd32f93e2e847b89880faf97f67f1915ea934c
adnroboticsfr <adnroboticsfr@gmail.com>
2024-09-16 21:11:31 +0200

    add

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="smartpi-gpio",
-    version="1.0.0",
-    description="Gestion des GPIO pour Smart Pi One",
-    author="Votre Nom",
-    packages=find_packages(),
-    scripts=['bin/gpio-smpi'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: POSIX :: Linux",
+    name='smartpi-gpio',
+    version='1.0.0',
+    packages=['gpio'],
+    entry_points={
+        'console_scripts': [
+            'gpio-smpi=bin.gpio-smpi:main',
+        ],
+    },
+    install_requires=[
+        # Add your dependencies here
     ],
 )
