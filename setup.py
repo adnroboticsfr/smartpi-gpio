d2bd4b4835d5a2980eb58d20ba4bfbd51680fea5
adnroboticsfr <adnroboticsfr@gmail.com>
2024-09-16 18:28:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
-import subprocess
+import shutil
+import os
+import sys
 
-class InstallCommand(_install):
-    def run(self):
-        _install.run(self)
-        subprocess.call(['python3', 'post_install.py'])
+def install_script():
+    script_name = 'gpio'
+    source_script = os.path.join(os.path.dirname(__file__), 'bin', f'{script_name}.py')
+    target_path = '/usr/bin/' + script_name
+
+    if not os.path.isfile(source_script):
+        print(f"Source script {source_script} not found.")
+        sys.exit(1)
+
+    try:
+        shutil.copy(source_script, target_path)
+        os.chmod(target_path, 0o755)
+        print(f'Successfully installed {script_name} to /usr/bin and made it executable.')
+    except Exception as e:
+        print(f'Error: {e}')
+        sys.exit(1)
 
 setup(
-    name="smart_pi_gpio",
-    version="1.0.0",
-    description="GPIO control for Smart Pi One",
-    author="Your Name",
+    name='smart-pi-gpio',
+    version='1.0.0',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'gpio=bin.gpio:main',
-        ],
-    },
-    install_requires=[
-        # Add any required dependencies here
-    ],
-    python_requires='>=3.6',
+    scripts=['bin/gpio.py'],  # Specifie le script à installer
     cmdclass={
-        'install': InstallCommand,
+        'install': install_script  # Appelle le script d'installation personnalisé
     },
 )
-
