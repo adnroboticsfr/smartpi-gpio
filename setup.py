420a2fc3356d899c00c620618d0de9e1c95f65ac
adnroboticsfr <adnroboticsfr@gmail.com>
2024-09-20 01:59:56 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
+import os
 
 class PostInstallCommand(install):
+    """Post-installation for setup script to run activate_interfaces.sh."""
     def run(self):
-        install.run(self)
-        subprocess.run(['python3', 'post_install.py'])
+        install.run(self)  # Run the default installation
+        try:
+            # Running the post-installation script to activate interfaces
+            script_path = os.path.join(os.path.dirname(__file__), 'bin/activate_interfaces.sh')
+            subprocess.run(['sudo', 'bash', script_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error running post-installation script: {e}")
 
 setup(
     name="smartpi-gpio",
@@ -13,8 +20,7 @@ setup(
     description="GPIO management for Smart Pi One",
     author="ADNroboticsfr",
     packages=find_packages(),
-    scripts=['bin/gpio',
-             'bin/activate_interfaces.sh'],
+    scripts=['bin/gpio'],  # Only include `bin/gpio` in the scripts section
     install_requires=[
         'Flask>=2.0.0',
         'Pillow>=8.0.0',
