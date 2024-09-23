85582a88a920a1d16c1184c2c4dd5b9ab442214b
adnroboticsfr <adnroboticsfr@gmail.com>
2024-09-23 05:53:37 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,14 +1,22 @@
+ 
 import shutil
 import os
 
 def main():
-    source = 'bin/enable_interfaces'
+    # Obtenir le chemin absolu du fichier source
+    source = os.path.join(os.path.dirname(__file__), 'bin', 'enable_interfaces')
     destination = '/usr/local/bin/enable_interfaces'
+    
+    # Vérifier si le fichier source existe
+    if not os.path.isfile(source):
+        print(f"Erreur : Le fichier source '{source}' n'existe pas.")
+        return
     
     # Copier le fichier
     shutil.copy(source, destination)
     # Rendre le fichier exécutable
     os.chmod(destination, 0o755)
+    print(f"Fichier copié et rendu exécutable : {destination}")
 
 if __name__ == '__main__':
     main()
