bb47d28b4db78e88334e0999c40115d328f8920c
adnroboticsfr <adnroboticsfr@gmail.com>
2024-09-23 05:43:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
 import os
-import shutil
 
 class PostInstallCommand(install):
     """Post-installation script to execute activate_interfaces.sh and enable_features.sh."""
@@ -12,33 +11,27 @@ class PostInstallCommand(install):
             # Paths to the scripts in the bin/ directory
             activate_script_path = 'bin/activate_interfaces.sh'
             enable_script_path = 'bin/enable_interfaces.sh'
-            
-            # Destination paths in /usr/local/bin/
-            activate_dest_path = '/usr/local/bin/activate_interfaces.sh'
-            enable_dest_path = '/usr/local/bin/enable_interfaces.sh'
 
-            print(f"Copying activation script to {activate_dest_path}")
-            print(f"Copying enabling features script to {enable_dest_path}")
-
+            print(f"Script path for activation: {activate_script_path}")
+            print(f"Script path for enabling features: {enable_script_path}")
+        
             # Check if activate_interfaces.sh exists
             #if os.path.exists(activate_script_path):
-                # Copy the script to /usr/local/bin/
-                #shutil.copy(activate_script_path, activate_dest_path)
                 # Make the script executable
-                #.run(['chmod', '+x', activate_dest_path], check=True)
+                #subprocess.run(['chmod', '+x', activate_script_path], check=True)
 
+                # Execute the script
+                #subprocess.run(['bash', activate_script_path], check=True)
             #else:
                 #print(f"Error: The file {activate_script_path} is not found.")
 
             # Check if enable_features.sh exists
             if os.path.exists(enable_script_path):
-                # Copy the script to /usr/local/bin/
-                shutil.copy(enable_script_path, enable_dest_path)
                 # Make the script executable
-                subprocess.run(['chmod', '+x', enable_dest_path], check=True)
+                subprocess.run(['chmod', '+x', enable_script_path], check=True)
 
                 # Execute the script
-                subprocess.run(['bash', enable_dest_path], check=True)
+                subprocess.run(['bash', enable_script_path], check=True)
             else:
                 print(f"Error: The file {enable_script_path} is not found.")
 
